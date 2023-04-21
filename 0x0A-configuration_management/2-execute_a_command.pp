4addd60bb1224e9abb54e115bbfe1e08c22a5abb
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-21 10:45:06 -0700

    requirement

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,6 @@
 # execute a command
-# the pkill command kills any orocess with the name killmenow
-# onlyif parameter specifies that the pgrep command should be used to check
-# if a process with the name killmenow is running before attempting to kill it
 
 exec { 'killmenow':
   command => 'pkill -f killmenow',
-  onlyif  => 'pgrep -f killmenow',
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
