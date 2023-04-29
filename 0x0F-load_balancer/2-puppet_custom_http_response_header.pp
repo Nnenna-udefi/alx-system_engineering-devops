ce910d8c0c8f7de107b7f75750d88aec97cca3d5
Nnenna Udefi <miriamudefi@gmail.com>
2023-04-29 08:37:38 -0700

    puuply_apply

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,27 @@
 # installs an nginx server and creates a custom http header response
 
 # install nginx
-class { 'nginx': }
-
-# custom header
-file { '/etc/nginx/nginx.conf':
-  content => "server_tokens off;\nadd_header X-Served-By $hostname;\n",
-  notify => Service['nginx'],
+exec { 'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-# restart nginx services when the header changes
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
+exec { 'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+# add custom header
+exec { 'add_header':
+  provider    => shell,
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$hostname\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+
+# restart nginx services when the header changes have been made
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
