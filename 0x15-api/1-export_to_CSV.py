51214ec512c94b1dedfa155fbc53690794bab798
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-22 07:19:59 -0700

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,7 +2,9 @@
 """
     Script that uses a restful api for a given employee ID,
     returns information about his/her TODO list progress.
+    and exports data in CSV format
 """
+import csv
 import requests
 import sys
 
@@ -22,12 +24,13 @@ def get_employee_todo_progress(employee_id):
     user = user_respons.json()
     employee_name = user['name']
 
-    # print the employee's todo list progress
-    print('Employee {} is done with tasks({}/{}):'
-          .format(employee_name, len(completed_tasks), len(todos)))
-
-    for task in completed_tasks:
-        print('\t{}'.format(task['title']))
+    # exports data to csv
+    filename = '{}.csv'.format(employee_id)
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for task in todos:
+            writer.writerow([employee_id, employee_name,
+                            task['completed'], task['title']])
 
 
 if __name__ == '__main__':
