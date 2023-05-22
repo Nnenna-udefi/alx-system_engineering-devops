566eed67789be7177a6e6541a0e801c72b37da01
Nnenna Udefi <miriamudefi@gmail.com>
2023-05-22 16:38:16 -0700

    json_format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,7 +2,9 @@
 """
     Script that uses a restful api for a given employee ID,
     returns information about his/her TODO list progress.
+    and exports data in JSON format
 """
+import json
 import requests
 import sys
 
@@ -20,13 +22,20 @@ def get_employee_todo_progress(employee_id):
     user_respons = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                                 .format(employee_id))
     user = user_respons.json()
-    employee_name = user['name']
+    username = user['username']
 
-    # print employee todo list
-    print('Employee {} is done with tasks({}/{}):'
-          .format(employee_name, len(completed_tasks), len(todos)))
-    for task in completed_tasks:
-        print('\t{}'.format(task['title']))
+    # exports data to json
+    data = {employee_id: [
+        {
+            "task": task["title"],
+            "completed": task["completed"],
+            "username": username
+        }
+        for task in todos
+    ]}
+    filename = '{}.json'.format(employee_id)
+    with open(filename, "w") as file:
+        json.dump(data, file)
 
 
 if __name__ == '__main__':
